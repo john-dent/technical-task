e0ae4952901141ffd6d2079ca788182faf975c0d
John <john.dent@gmail.com>
2021-07-30 23:53:10 +0100

    remove assignee relationship when removing assignees

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,6 +1,21 @@
 const Setting = ({ getSettings, setting, settingType }) => {
     // Delete Setting
     const deleteSetting = async () => {
+        if (settingType === 'assignees') {
+            const oldAssignees = await fetch(`http://localhost:5000/recordsWithAssignees?assigneeId=${setting.id}`)
+
+            const data = await oldAssignees.json()
+    
+            data.forEach((old) => (
+                fetch(`http://localhost:5000/recordsWithAssignees/${old.id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-type': 'application/json',
+                    }
+                })
+            ))
+        }
+
         await fetch(`http://localhost:5000/${settingType}/${setting.id}`, {
         method: 'DELETE',
         })
